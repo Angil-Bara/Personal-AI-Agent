a38fda45e3008e8adc315e4397b09acb0807310f
Angilbert Baraka <gbaraka103@gmail.com>
2025-11-15 22:31:43 -0500

    sql event schema

--- src/schema.sql
+++ src/schema.sql
@@ -18,4 +18,13 @@ CREATE TABLE emails (
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE -- Establish relationship with Users table
 );
 
--- 
+-- SQL script to create the Tasks/Events table
+CREATE TABLE events(
+    id SERIAL PRIMARY KEY, -- Unique identifier for each event
+    user_id INT NOT NULL, -- Foreign key to the users table
+    title VARCHAR(255) NOT NULL, -- Title of the event
+    description TEXT, -- Short description of the event
+    date DATE NOT NULL, -- Date of the event
+    time TIME NOT NULL, -- Time of the event
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE -- Establish relationship with Users table
+);
